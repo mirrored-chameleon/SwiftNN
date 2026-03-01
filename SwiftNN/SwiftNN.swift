da2ff770aa7fc0bbcd1ba75903134ab8bb78d4d8
Davyn Monagle <davyn@monagle.family>
2026-03-01 17:16:08 +1100

    Initial Commit

---
+++ SwiftNN/SwiftNN.swift
@@ -0,0 +1,9 @@
+//
+//  SwiftNN.swift
+//  SwiftNN
+//
+//  Created by Davyn Monagle on 1/3/2026.
+//
+
+import Foundation
+
---
+++ SwiftNNTests/SwiftNNTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNNTests.swift
+//  SwiftNNTests
+//
+//  Created by Davyn Monagle on 1/3/2026.
+//
+
+import Testing
+@testable import SwiftNN
+
+struct SwiftNNTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
