513053ff3058c000a3893a1a93a4ed908a232953
Davyn Monagle <davyn@monagle.family>
2026-03-01 19:15:09 +1100

    Most protocols added
    I added most of the protocols, and will continue to work on adding some basic models into the library as an example.

--- SwiftNN/SwiftNN.docc/SwiftNN.md
+++ SwiftNN/SwiftNN.docc/SwiftNN.md
@@ -1,10 +1,11 @@
 # ``SwiftNN``
 
-<!--@START_MENU_TOKEN@-->Summary<!--@END_MENU_TOKEN@-->
+SwiftNN is an open source, modular neural network framework in swift so that you can create custom neural nets purely in swift with no porting from other languages.
 
 ## Overview
 
-<!--@START_MENU_TOKEN@-->Text<!--@END_MENU_TOKEN@-->
+This project uses Surge, a high-performance Swift library for numerical and matrix operations.
+https://github.com/Jounce/Surge
 
 ## Topics
 
--- SwiftNN/SwiftNN.swift
+++ SwiftNN/SwiftNN.swift
@@ -6,4 +6,82 @@
 //
 
 import Foundation
+internal import Surge
+
+
+// MARK: Protocols
+
+/// A protocl that has 2 functions of `Encode` and `Decode` so that the model can take in different inputs and output different outputs.
+///
+/// There are 2 generics of `Input` and `Output` so that the model can take in different types you teach it, so it could be a `String` or an `Int` for instance.
+
+protocol Talent {
+    
+    /// Input type for the model
+    ///
+    /// Generic type for `Talent` so that the `Network` can take in any input that you want it to.
+    
+    associatedtype Input
+    
+    /// Output type for the model
+    ///
+    /// Generic type for `Talent` so that the `Network` can output any output you want it to.
+    
+    associatedtype Output
+    
+    /// `Encode` function inside of `Talent`
+    ///
+    /// The function is used so that the `Model` is able to encode any type you want it to.
+    
+    func encode(_ input: Input) -> Matrix<Double>
+    
+    
+    
+    func decode(_ output: Matrix<Double>) -> Output
+    
+}
+
+
+/// A protocol describing the basic requirements of a neural network.
+///
+/// Designed to be minimal, this protocol defines the core components
+/// shared by neural networks: weights, biases, and training behavior.
+///
+/// The weights and biases are represented as `Matrix<Double>` values.
+/// Implementations are expected to perform forward propagation for
+/// prediction and backpropagation during training.
+
+protocol Network {
+    /// Weights for the model to use
+    ///
+    /// A simple weights variable, for the `Network` to use to train and predict.
+    ///
+    /// It uses a `Matrix<Double>` type.
+    
+    var weights: Matrix<Double> { get set }
+    /// Biases for the model to use
+    ///
+    /// A simple bias variable for the `Network` to use to train and predict.
+    ///
+    /// It uses a `Matrix<Double>` type.
+    
+    var bias: Matrix<Double> { get set }
+    
+    /// `train()` function
+    ///
+    /// Takes in a `Matrix<Double>` as input, and a target input of the same type.
+    
+    func train(input: Matrix<Double>, target: Matrix<Double>)
+    
+    /// `predict()` function
+    ///
+    /// Takes in a `Matrix<Double>` type and returns a `Matrix<Double>`.
+    
+    func predict(input: Matrix<Double>) -> Matrix<Double>
+    
+}
+
+
+// This is so that the Network can also be called Model if that is more comfortable for users.
+typealias Model = Network
 
